405d917d082f1ac45aee2a9635c4f3d0ecb382d5
DimitrovVladimir <vmdimitrow@gmail.com>
2022-09-29 21:00:35 +0300

    nextStage

--- UIShop/AccessTheSystemWithNotLoggedUser.cs
+++ UIShop/AccessTheSystemWithNotLoggedUser.cs
@@ -1,5 +1,6 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using static System.Collections.Specialized.BitVector32;
 
@@ -20,23 +21,31 @@ namespace UIShop
         [Test, Category("Access the system")]
         public void AccessTheSystemWithNotLoggedUser()
         {
-           
-            webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[1]/a"));
-            IWebElement myWishlist = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[1]/a"));
-           
+
+
+
+            // web element Displayed
+            IWebElement navigationBar = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]"));
+            Boolean navibationBar_enabled = navigationBar.Enabled;
+            IWebElement header = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[2]/div/div/div"));
+            Boolean header_enabled = header.Enabled;
+
             Assert.Pass();
         }
         [Test, Category("Access the system")]
         public void AccessTheSystemWithLoggedinUser()
         {
 
-            webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[1]/a"));
+            IWebElement navigationBar = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]"));
+            Boolean navibationBar_enabled = navigationBar.Enabled;
+            IWebElement header = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[2]/div/div/div"));
+            Boolean header_enabled = header.Enabled;
             Assert.Pass();
         }
         [Test, Category("Add product to Cart")]
         public void VerifyAddingTheProductToCart()
         {
-            //LoginUser
+            //LoginUserDone
             webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[2]/a")).Click();
             webDriver.FindElement(By.Id("username")).SendKeys("pnrkouwsohyhlgaavs@bvhrk.com");
             webDriver.FindElement(By.Id("password")).SendKeys("Pineapple250590");
@@ -68,10 +77,87 @@ namespace UIShop
             //fall or assert
             Assert.Pass();
         }
-        [TearDown]
-        public void Close() 
+        [Test, Category("Navigation bar")]
+        public void VerifyNavigationBarSectionsWithNotLoggedUser()
         {
-            webDriver.Close();
+            IWebElement myWishList = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[1]/a"));
+            Boolean myWishList_enabled = myWishList.Enabled;
+            IWebElement myAccount = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[2]/a"));
+            Boolean myAccount_enabled = myAccount.Enabled;
+            IWebElement checkOut = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[3]/a"));
+            Boolean checkOut_enabled = checkOut.Enabled;
+            IWebElement searchField = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[2]/div/a"));
+            Boolean searchField_enabled = searchField.Enabled;
+            IWebElement email = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[1]/li[1]/a"));
+            Boolean email_enabled = email.Enabled;
+            IWebElement telephone = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[1]/li[2]/a"));
+            Boolean telephone_enabled = telephone.Enabled;
+            IWebElement cart = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[2]/div/ul/li/a/span/span[2]"));
+            Boolean cart_enabled = cart.Enabled;
+
+            Assert.Pass();
         }
+        [Test, Category("Navigation bar")]
+        public void VerifyNavigationBarSectionsWithLoggedInUser()
+        {
+            webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[2]/a")).Click();
+            webDriver.FindElement(By.Id("username")).SendKeys("pnrkouwsohyhlgaavs@bvhrk.com");
+            webDriver.FindElement(By.Id("password")).SendKeys("Pineapple250590");
+            webDriver.FindElement(By.Name("login")).Click();
+            webDriver.FindElement(By.XPath("/html/body/div[1]/header/div[2]/div/div/div/div/a/img")).Click();
+
+            IWebElement myWishList = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[1]/a"));
+            Boolean myWishList_enabled = myWishList.Enabled;
+            IWebElement myAccount = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[2]/a"));
+            Boolean myAccount_enabled = myAccount.Enabled;
+            IWebElement checkOut = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[3]/a"));
+            Boolean checkOut_enabled = checkOut.Enabled;
+            IWebElement searchField = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[2]/div/a"));
+            Boolean searchField_enabled = searchField.Enabled;
+            IWebElement email = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[1]/li[1]/a"));
+            Boolean email_enabled = email.Enabled;
+            IWebElement telephone = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[1]/li[2]/a"));
+            Boolean telephone_enabled = telephone.Enabled;
+            IWebElement cart = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[2]/div/ul/li/a/span/span[2]"));
+            Boolean cart_enabled = cart.Enabled;
+
+
+            Assert.Pass();
+        }
+        [Test, Category("Navigation bar")]
+        public void VerifyCheckoutSectionWithNotLoggedUser()
+        {
+            IWebElement checkOut = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[3]/a"));
+            checkOut.Click();
+            //users is redirected to the shoping cart.
+
+
+
+            Assert.Pass();
+        }
+        [Test, Category("Navigation bar")]
+        public void VerifyCheckoutSectionWithLoggedInUser()
+        {
+            webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[2]/a")).Click();
+            webDriver.FindElement(By.Id("username")).SendKeys("pnrkouwsohyhlgaavs@bvhrk.com");
+            webDriver.FindElement(By.Id("password")).SendKeys("Pineapple250590");
+            webDriver.FindElement(By.Name("login")).Click();
+            webDriver.FindElement(By.XPath("/html/body/div[1]/header/div[2]/div/div/div/div/a/img")).Click();
+
+            IWebElement checkOut = webDriver.FindElement(By.XPath("/html/body/div[2]/header/div[1]/div/ul[2]/li[3]/a"));
+            checkOut.Click();
+            //users is redirected to the shoping cart.
+
+
+
+            Assert.Pass();
+        }
+        [TearDown]
+          public void Close()
+            {
+                webDriver.Close();
+            }
+
     }
+    
 }
